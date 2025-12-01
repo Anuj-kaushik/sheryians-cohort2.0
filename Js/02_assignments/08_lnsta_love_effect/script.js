81b35064b7ce2b9f144ab1a6842a551c84b071ec
Anuj <anujkaushik2k02@gmail.com>
2025-12-01 17:55:23 +0530

    day 46 insta love effect completed

---
+++ Js/02_assignments/08_lnsta_love_effect/script.js
@@ -0,0 +1,18 @@
+let like = document.querySelector("#love");
+let imgContainer = document.querySelector("#imgContainer");
+
+imgContainer.addEventListener("click", () => {
+  console.log("hello");
+
+  like.style.transform = "translate(-50%, -50%) scale(2) rotate(0deg)";
+  setTimeout(() => {
+    like.style.top = -20 + "%";
+    like.style.transform = "translate(-50%, -50%) scale(2) rotate(60deg)";
+  }, 1000);
+  setTimeout(() => {
+    like.style.transform = "translate(-50%, -50%) scale(0) rotate(-60deg)";
+  }, 1100);
+  setTimeout(() => {
+    like.style.top = 50 + "%";
+  }, 2000);
+});
